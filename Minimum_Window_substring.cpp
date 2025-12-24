b117a5e30c23dbb962eb82c57e47d599e0faf91b
DhruvKumar2917 <dhruvkumar6297@gmail.com>
2025-12-24 19:24:28 +0530

    Largest Sum Minimised

--- Minimum_Window_substring.cpp
+++ Minimum_Window_substring.cpp
@@ -33,3 +33,37 @@ public:
         }
     
 };
+
+
+class Solution {
+    int numberOfArray(vector<int> &nums,int mid){
+        int arr=1;
+        long long sum=0;
+        for(int i=0;i<nums.size();i++){
+            if((sum+nums[i])<=mid){
+                sum+= nums[i];
+
+            }
+            else{
+                sum =nums[i];
+                arr++;
+            }
+        }
+        return arr;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        if(k>nums.size()) return -1;
+         int low =*max_element(nums.begin(),nums.end());
+        int high =accumulate(nums.begin(),nums.end(),0);
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(numberOfArray(nums,mid)>k) low=mid+1;
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+        
+    }
+};
